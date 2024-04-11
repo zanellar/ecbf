fixed bug symbolic derivative hamiltonian control; added double pendulum [tested only free motion]; added new plots kinetic energy limit

ecbf/scripts/control.py
@@ -76,11 +76,11 @@ class Controller():
 
         ########### Symbolic State ###########
   
-        _x, _y = sp.symbols('x y')  # define symbolic representation
-        self._state = sp.Matrix([_x, _y])  # row vector
+        _q, _p = sp.symbols('q p')  # define symbolic representation
+        self._state = sp.Matrix([_q, _p])  # row vector
 
-        _xd, _yd = sp.symbols('xd yd')
-        self._target_state = sp.Matrix([_xd, _yd])  # row vector
+        _qd, _pd = sp.symbols('qd pd')
+        self._target_state = sp.Matrix([_qd, _pd])  # row vector
  
         ########### Dynamics ###########
 
@@ -130,6 +130,12 @@ class Controller():
             _dx_H = sp.Matrix([self.model._H]).jacobian(self._state).T
               
             # Lie derivatives of CBF  w.r.t f(x)=F*dHdx(x)
+            print("@@@@@@@@@@@@@@@@@@@@@@2")
+            print("_cbf", _cbf)
+            print("_dx_cbf", _dx_cbf)
+            print("self.model._F",self.model._F)
+            print("self.model._H",self.model._H)
+            print("_dx_H",_dx_H)
             self._dLie_f_cbf = _dx_cbf.T @ self.model._F @ _dx_H
 
             # Lie derivatives of CBF  w.r.t g(x)=G
@@ -138,7 +144,9 @@ class Controller():
             # Make the symbolic functions callable
             self.dLie_f_cbf = lambdify([self._state], self._dLie_f_cbf)
             self.dLie_g_cbf = lambdify([self._state], self._dLie_g_cbf)
- 
+
+        self.cbf_constrant_value = None
+
         ########### Solver ###########
         opts_setting = {
             'ipopt.max_iter': 200,
@@ -160,6 +168,7 @@ class Controller():
         self.slackt = np.zeros((1, self.time_steps))
         self.clf_t = np.zeros((1, self.time_steps))
         self.cbf_t = np.zeros((1, self.time_steps))
+        self.cbf_ct = np.zeros((1, self.time_steps))
         self.openloop_energy_t = np.zeros((1, self.time_steps))
         self.closeloop_energy_t = np.zeros((1, self.time_steps))
  
@@ -227,7 +236,7 @@ class Controller():
             dLie_f_cbf = self.dLie_f_cbf(current_state)
             dLie_g_cbf = self.dLie_g_cbf(current_state)
 
-            # Lfh + Lgh * u + gamma * h >= 0
+            # Lfh + Lgh * u + gamma * h >= 0 
             self.opti.subject_to(dLie_f_cbf + dLie_g_cbf @ self.u + self.cbf_gamma * cbf >= 0)
 
         # optimize the Qp problem
@@ -241,11 +250,14 @@ class Controller():
                 slack = sol.value(self.slack)
             else:
                 slack = None
+                
+            if self.cbf_tool is not None:
+                self.cbf_constrant_value = dLie_f_cbf + dLie_g_cbf @ [[optimal_control]] + self.cbf_gamma * cbf
 
             return optimal_control, slack, clf, cbf, self.feasible
         
-        except:
-
+        except Exception as e:
+            print(e)
             print(self.opti.return_status())
             self.feasible = False
 
@@ -283,6 +295,7 @@ class Controller():
             self.slackt[:, t] = delta
             self.clf_t[:, t] = clf
             self.cbf_t[:, t] = cbf 
+            self.cbf_ct[:, t] = self.cbf_constrant_value
 
             # Compute the energy of the open-loop system 
             H = self.model.get_energy(self.current_state + self.target_state) 
@@ -331,7 +344,7 @@ class Controller():
 
     #######################################################################################################################
 
-    def plot_phase_trajectory(self, add_safe_set=True, plot_end_state = True, state_range=[-20, 20], show=True, save=False, figure=None, name = '', color='blue', arrow_skip=10, arrow_clif=0):
+    def plot_phase_trajectory(self, add_safe_set=True, plot_end_state = True, state_range=[-20, 20], show=True, save=False, figure=None, name = '', color='blue', arrow_skip=10, arrow_clif=np.inf, arrow_index=-1):
         if figure is None:
             plt.figure()
         else:
@@ -349,9 +362,9 @@ class Controller():
 
         # Add arrows to indicate the direction of motion
         for i in range(0, len(q_traj) - 1, arrow_skip):  # Stop one step earlier
-            if i>arrow_clif:
+            if i>arrow_clif or arrow_index == i:
                 plt.annotate('', xy=(q_traj[i+1], p_traj[i+1]), xytext=(q_traj[i], p_traj[i]),
-                            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle='Simple,tail_width=0.5,head_width=0.5,head_length=0.5'))    
+                            arrowprops=dict(facecolor=color, edgecolor=color, arrowstyle='Simple,tail_width=0.5,head_width=0.5,head_length=0.5'))      
                        
         # Plot the initial and final with a higher z-order without a label
         plt.scatter([q_traj[0]], [p_traj[0]], color='black', zorder=5)
@@ -604,6 +617,34 @@ class Controller():
 
         if save:
             file_name = 'control.png' if name == '' else f'control_{name}.png'
+            plt.savefig(os.path.join(PLOTS_PATH, file_name), format='png', dpi=300)  
+
+        
+    #######################################################################################################################
+
+    def plot_cbf_constraint(self, show=True, save=False, figure=None, name = '', color='blue'):
+        if figure is None:
+            plt.figure()
+        else:
+            plt.sca(figure)
+             
+        t = np.arange(0, self.T, self.dt)
+        constraint = self.cbf_ct.flatten()
+
+        plt.grid() 
+        plt.plot(t, constraint, linewidth=3, label=name, linestyle='-', color=color) 
+
+        plt.grid(True)
+        #plt.title('constraint')
+        plt.xlabel('time [s]') 
+        plt.ylabel('constraint')
+        plt.legend(loc='upper left')
+ 
+        if show:
+            plt.show()
+
+        if save:
+            file_name = 'constraint.png' if name == '' else f'constraint_{name}.png'
             plt.savefig(os.path.join(PLOTS_PATH, file_name), format='png', dpi=300)  
 
         

ecbf/scripts/phsys.py
@@ -41,29 +41,42 @@ class PHSystemCanonic():
         self.num_states = 2 if np.array(D).ndim < 1 else np.array(D).shape[0]*2  # Number of states
         self.num_inputs = 1 if np.array(B).ndim < 1 else np.array(B).shape[1]    # Number of inputs
 
+        # Check if the system is scalar
+        self.scalar = True if np.array(D).ndim < 1 else False
+
         # Define the symbolic variables
-        q, p = sp.symbols('q p')
+        if self.scalar:
+            q, p = sp.symbols('q p')
+        else:
+            q = sp.Matrix(sp.symbols(f'q:{self.num_states//2}'))
+            p = sp.Matrix(sp.symbols(f'p:{self.num_states//2}'))
+
         self.q = q
         self.p = p
         x = sp.Matrix([q, p]) 
 
         # Define the Hamiltonian
         self._H = self.K() + self.V() 
-        print(self._H)
+        
         self.H = lambdify([q, p], self._H)
   
         # Define the gradient of the Hamiltonian 
         _dHdq = sp.diff(self._H, q)
-        print(_dHdq)
         _dHdp = sp.diff(self._H, p)
         self._dH = sp.Matrix([_dHdq, _dHdp]) 
         self.dH = lambdify([q, p], self._dH)
 
         # Define the state transition matrix F 
-        self._F = np.array([[0, 1], [-1, -self._D]])
+        if self.scalar:
+            self._F = np.array([[0, 1], [-1, -self._D]])
+        else:
+            self._F = np.block([[np.zeros((self.num_states//2, self.num_states//2)), np.eye(self.num_states//2)], [-np.eye(self.num_states//2), -np.array(self._D)]])
 
         # Define the input matrix G 
-        self._G = np.array([[0], [self._B]])
+        if self.scalar:
+            self._G = np.array([[0], [self._B]])
+        else:
+            self._G = np.block([[np.zeros((self.num_states//2, self.num_inputs))], [np.array(self._B)]])
 
         # Make Kinetic and Potential energy functions callable
         self.K = lambdify([q, p], self.K())
@@ -73,14 +86,26 @@ class PHSystemCanonic():
         '''
         This function returns the energy of the system.
         ''' 
-        return self.H(x[0], x[1])
+        q = x[0] if self.scalar else x[0:self.num_states//2]
+        p = x[1] if self.scalar else x[self.num_states//2:] 
+        return self.H(q,p)
     
     def dynamics(self, x, u):
         '''
         This function returns the dynamics of the system.
         ''' 
+        q = x[0] if self.scalar else x[0:self.num_states//2]
+        p = x[1] if self.scalar else x[self.num_states//2:] 
+        
+        # print("@@@@@@@@")
+        # print(q)
+        # print(p)
+        # print(self.dH(q,p))
+        # print(self._F)
+        # print(self._G)
+        # print(u)
 
-        dx = self._F @ self.dH(x[0], x[1]) + self._G @ u
+        dx = self._F @ self.dH(q,p) + self._G @ u
 
         return dx    
 
@@ -102,7 +127,7 @@ class PHSystemCanonic():
         k2 = self.dynamics(x + self.dt/2 * k1, u)
         k2 = k2.reshape(state_shape)
 
-        k3= self.dynamics(x + self.dt/2 * k2, u)
+        k3 = self.dynamics(x + self.dt/2 * k2, u)
         k3 = k3.reshape(state_shape)
 
         k4 = self.dynamics(x + self.dt * k3, u)
@@ -121,7 +146,9 @@ class PHSystemCanonic():
 
         # Integrate the system
         x_next = self._integrate_rk4(x, u) 
-        y = self._G.T @ self.dH(x_next[0], x_next[1])
+        q = x_next[0] if self.scalar else x_next[0:self.num_states//2]
+        p = x_next[1] if self.scalar else x_next[self.num_states//2:]
+        y = self._G.T @ self.dH(q,p)
 
         if self.verbose:
             print(f'q: {x_next[0]}')

ecbf/scripts/sim.py
@@ -35,8 +35,10 @@ class Simulator():
             x, y = self.model.step(x, u)
             x_traj.append(x)
             y_traj.append(y)
-            K_traj.append(self.model.K())
-            V_traj.append(self.model.V())
+            q = x[0] if self.model.scalar else x[0:self.model.num_states//2]
+            p = x[1] if self.model.scalar else x[self.model.num_states//2:]
+            K_traj.append(self.model.K(q,p))
+            V_traj.append(self.model.V(q,p))
             print(f'step {i+1}/{n_steps}')
         return x_traj, y_traj
     
@@ -147,12 +149,10 @@ class Simulator():
         # Calculate the energy of each state
         energy_traj = []
         for state in state_traj:
-            q = state[0]
-            p = state[1]
-            state = np.array([q, p])
-            self.model.q = q
-            self.model.p = p
-            energy = self.model.K() + self.model.V()
+            q = state[0] if self.model.scalar else state[0:self.model.num_states//2]
+            p = state[1] if self.model.scalar else state[self.model.num_states//2:]
+            state = np.array([q, p]) 
+            energy = self.model.K(q,p) + self.model.V(q,p)
             energy_traj.append(energy) 
 
         # Plot the energy trajectory